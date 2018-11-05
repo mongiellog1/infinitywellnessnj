7bc596191144c6bbfadae02db7005f29299c902f
Kevin Mongiello <kmong92@gmail.com>
2018-11-04 23:24:22 -0500

    Test log during build

--- build.js
+++ build.js
@@ -4,3 +4,9 @@ SNIPCART_API_KEY=${process.env.SNIPCART_API_KEY}
 IWNJ_address=${process.env.IWNJ_ADDRESS}
 SHOP_address=${process.env.SHOP_ADDRESS}
 \n`)
+
+console.log("BUILDING !!!!!!!!!!!!!!!!!!!!!!!")
+console.log("BUILDING !!!!!!!!!!!!!!!!!!!!!!!")
+console.log("BUILDING !!!!!!!!!!!!!!!!!!!!!!!")
+console.log("BUILDING !!!!!!!!!!!!!!!!!!!!!!!")
+console.log("BUILDING !!!!!!!!!!!!!!!!!!!!!!!")
