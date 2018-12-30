1ed2ff2fe936cc4cde85cf18eccd98ea08bd8573
Kevin Mongiello <kmong92@gmail.com>
2018-12-29 22:04:07 -0500

    Replace PayPal buttons; Add Group EftService

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout/index";
-import PageDivider from "../components/page_divider";
+import Divider from "../components/page_divider";
 import { Testimonial1 } from "../components/testimonial";
 import HeroSection from "../components/hero_section";
+
+const styles = {
+  ul: {
+    fontSize: "20px",
+    listStyleType: "disc",
+    marginLeft: "1.5rem",
+  },
+  li: {
+    marginBottom: 0,
+    lineHeight: "24px"
+  }
+};
 
 const PayPage = () => (
   <Layout>
@@ -11,28 +23,52 @@ const PayPage = () => (
       title="Services"
       Background={<div className="background-cover-parent background--services-hero" />}
     />
-
     <div className="slanted-section-decorator slanted-section-decorator--eft"/>
     <div className="container container--max-width">
       <div className="section">
         <h2 className="purple">Single Sessions</h2>
-        <ul style={{ fontSize: "20px", listStyleType: "disc", marginLeft: "1.5rem" }}>
-          <li style={{ marginBottom: 0, lineHeight: "24px"}}>Myofascial Release</li>
-          <li style={{ marginBottom: 0, lineHeight: "24px"}}>Emotional Freedom Techniques</li>
-          <li style={{ marginBottom: 0, lineHeight: "24px"}}>Yoga & Meditation</li>
-          <li style={{ marginBottom: 0, lineHeight: "24px"}}>Sound Healing & Gratitude</li>
+        <ul style={styles.ul}>
+          <li style={styles.li}>Myofascial Release</li>
+          <li style={styles.li}>Emotional Freedom Techniques</li>
+          <li style={styles.li}>Yoga & Meditation</li>
+          <li style={styles.li}>Sound Healing & Gratitude</li>
         </ul>
         <h3>Any Single Session - <span className="green">$120</span></h3>
         <p></p>
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_s-xclick" />
-          <input type="hidden" name="hosted_button_id" value="7RK4SYFN86H5E" />
+          <input type="hidden" name="hosted_button_id" value="9J9BD37J8PGZY" />
           <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-          <img alt="Purchase Single Myo Fascial Release (MFR) session" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
         </form>
       </div>
-    </div>
-    <div className="container container--max-width">
+
+      <Divider showLogo={false} />
+
+      <div className="section">
+        <div className="page-anchor__container">
+          <div id="group-eft" className="page-anchor__anchor"/>
+        </div>
+        <h2 className="purple">Group EFT Class</h2>
+        <p>
+          Group EFT classes are extremely powerful and versatile enough to
+          give every participant a chance to tap into their underlying patterns and clear up extra space!
+        </p>
+        <p>
+          For more information, visit the <Link className="inline-anchor"to="/emotional-freedom-techniques#group-eft">EFT page!</Link>
+        </p>
+        <h3>Price - <span className="green">$153</span></h3>
+        <p></p>
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+          <input type="hidden" name="cmd" value="_s-xclick" />
+          <input type="hidden" name="hosted_button_id" value="UWSBZU482BKQW" />
+          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+      </div>
+
+      <Divider showLogo={false} />
+
       <div className="page-anchor__container">
         <div id="basic-package" className="page-anchor__anchor"/>
       </div>
@@ -49,12 +85,13 @@ const PayPage = () => (
         <h3>Price - <span className="green">$560</span></h3>
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_s-xclick" />
-          <input type="hidden" name="hosted_button_id" value="FU97Y7HA9GEPU" />
+          <input type="hidden" name="hosted_button_id" value="E3XS8KYQPGF7W" />
           <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
           <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
         </form>
+
       </div>
-      <PageDivider showLogo={false} />
+      <Divider showLogo={false} />
       <div className="page-anchor__container">
         <div id="transformation-package" className="page-anchor__anchor"/>
       </div>
@@ -72,12 +109,13 @@ const PayPage = () => (
         <h3>Price - <span className="green">$1080</span></h3>
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_s-xclick" />
-          <input type="hidden" name="hosted_button_id" value="88N98VWWPFWHE" />
+          <input type="hidden" name="hosted_button_id" value="PTJLKQWVJNVUG" />
           <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-          <img alt="Purchase Transformation Series Package" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
         </form>
+
       </div>
-      <PageDivider showLogo={false} />
+      <Divider showLogo={false} />
       <div className="page-anchor__container">
         <div id="divine-inner-child-package" className="page-anchor__anchor"/>
       </div>
@@ -96,10 +134,11 @@ const PayPage = () => (
         <h3>Price - <span className="green">$3240</span></h3>
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_s-xclick" />
-          <input type="hidden" name="hosted_button_id" value="9SVZXTPMSLWNS" />
+          <input type="hidden" name="hosted_button_id" value="DZEA8B4Z6VASW" />
           <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-          <img alt="Purchase Divine Mothering Package" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
         </form>
+
       </div>
     </div>
     <Testimonial1 />
