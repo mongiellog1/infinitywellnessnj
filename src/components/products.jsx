41533e8890e80399a67589a15d636d7dae02b6d2
Kevin Mongiello <kmong92@gmail.com>
2019-01-01 21:01:37 -0500

    Update shop text

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -78,12 +78,17 @@ class ProductCard extends React.Component {
 const Products = ({ products, SHOP_ADDRESS, mandalaImages }) => (
   <div className="container container--pad-top">
     <h1 style={{ marginBottom: "1rem"}}>Mandala Cards</h1>
-    <p className="light-green">All the mandala images are created by hand. </p>
-    <p className="light-green">All mandala cards are 4 ½ by 6 inches when folded, or 9 x 6 inches when open. Mandala art is on the front side of the card, and the mandala title and a message is on the back of the card. The insides are left blank for you to write in, as you like. </p>
-    <p className="light-green">All cards are printed on 30% recycled paper, envelopes are 30% recycled. Mail order packaging is made from recycled material as well. </p>
-    <p className="light-green" style={{ marginBottom: "2rem", textDecoration: "underline", fontSize: "1.1em" }}><b>A minimum purchase of $20 is required. All packages have a $5 shipping and handling fee.
-</b></p>
-
+    <p className="light-green" style={{ marginBottom: "2.5rem" }}>All the mandala images are created by hand.</p>
+    <p className="light-green" style={{
+      marginBottom: "2rem",
+      textDecoration: "underline",
+      fontSize: "1.1em",
+      textAlign: "center"
+    }}>
+      <b>
+        A minimum purchase of $20 is required. All packages have a $5 shipping and handling fee.
+      </b>
+  </p>
     <div className="shop-products">
       {
         products.map((product, idx) => (
@@ -96,6 +101,8 @@ const Products = ({ products, SHOP_ADDRESS, mandalaImages }) => (
         ))
       }
     </div>
+    <p className="light-green" style={{marginTop: "2rem" }}>All mandala cards are 4 ½ by 6 inches when folded, or 9 x 6 inches when open. Mandala art is on the front side of the card, and the mandala title and a message is on the back of the card. The insides are left blank for you to write in, as you like. </p>
+    <p className="light-green">All cards are printed on 30% recycled paper, envelopes are 30% recycled. Mail order packaging is made from recycled material as well. </p>
   </div>
 );
 
